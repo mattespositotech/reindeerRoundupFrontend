e374a936fd87145d08669cd2944a5c6e1d107eac
mattespositotech <matt@teamduality.dev>
2024-08-29 20:27:23 -0500

    Display Check

--- src/app/components/roundups/ActionsToolbar.tsx
+++ src/app/components/roundups/ActionsToolbar.tsx
@@ -1,8 +1,9 @@
 import { Button, Icon } from "semantic-ui-react"
 import { Roundup } from "../../types/RoundupTypes"
-import { status } from "../../enums/RoundupEnums"
+import { status as roundupStatus } from "../../enums/RoundupEnums"
 import { useSetAllParticipantsToAccepted } from "../../data/roundup"
 import { useNavigate } from "react-router-dom"
+import { status as userStatus } from "../../enums/UserEnums"
 
 type ActionsToolbarProps = {
     roundup: Roundup
@@ -11,10 +12,14 @@ export default function ActionsToolbar({ roundup }: ActionsToolbarProps) {
     const navigate = useNavigate();
     const { mutate, loading } = useSetAllParticipantsToAccepted();
 
+    // replace with env check
+    const testEnv = true;
+
+    const displayAcceptBtn = testEnv && roundup.participants.some(part => part.status !== userStatus.accepted)
 
     return (
         <div>
-            <Button icon labelPosition="left" color='green' onClick={() => {
+            {displayAcceptBtn && <Button icon labelPosition="left" color='green' onClick={() => {
                 mutate(roundup._id);
                 navigate(0)
             }
@@ -22,13 +27,13 @@ export default function ActionsToolbar({ roundup }: ActionsToolbarProps) {
                 loading={loading}>
                 <Icon name='check circle' />
                 Set Everyone To Accepted
-            </Button>
-            {roundup.status === status.inProgress &&
+            </Button>}
+            {roundup.status === roundupStatus.inProgress &&
                 <Button icon labelPosition="left" color='blue'>
                     <Icon name='shipping fast' />
                     Launch Early
                 </Button>}
-            {roundup.status === status.complete &&
+            {roundup.status === roundupStatus.complete &&
                 <Button icon labelPosition="left" color='purple'>
                     <Icon name='eye' />
                     See Matches
